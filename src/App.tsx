bf5f00d69c77e3eb7eaab89f0379c88c6ffe9743
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-09-13 11:59:00 +0200

    feat: :sparkles: Add router and context providers to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import { ExploreContextProvider } from './context/exploreContext';
+import { TimelineContextProvider } from './context/timelineContext';
+import { UserContextProvider } from './context/userContext';
+import { useUser } from './hooks/useUser';
+import ExplorePage from './pages/ExplorePage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
+  const { isLogged } = useUser();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Devs Social Web</p>
-      </header>
-    </div>
+    <UserContextProvider>
+      <TimelineContextProvider>
+        <ExploreContextProvider>
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                {isLogged ? <Redirect to="/home" /> : <Redirect to="/login" />}
+              </Route>
+              <Route exact path="/home">
+                <HomePage />
+              </Route>
+              <Route exact path="/login">
+                <LoginPage />
+              </Route>
+              <Route exact path="/explore">
+                <ExplorePage />
+              </Route>
+            </Switch>
+          </Router>
+        </ExploreContextProvider>
+      </TimelineContextProvider>
+    </UserContextProvider>
   );
 }
 
