08e4d76c64c8c318c20fc05d3c6bc2b1b8fde98f
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-09-13 18:56:43 +0200

    feat: :lipstick: Update post card style

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,20 +1,61 @@
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import { formatDistance } from 'date-fns';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../context/userContext';
 import { Post } from '../types';
 
-type PostCardPropd = {
+type PostCardProp = {
   post: Post;
 };
 
-const PostCard = ({ post }: PostCardPropd) => {
+const PostCard = ({ post }: PostCardProp) => {
+  const { user } = useContext(UserContext);
+
   return (
-    <div>
-      <Link to={`/user/${post.authorId}`}>
-        <h2>{post.authorName}</h2>
+    <Card>
+      <Link
+        to={`/user/${post.authorId}`}
+        style={{ textDecoration: 'none', color: 'inherit' }}
+      >
+        <CardHeader
+          avatar={
+            <Avatar
+              alt={post.authorName}
+              src={`http://avatars.dicebear.com/api/bottts/${post.authorUsername}.svg`}
+            />
+          }
+          title={post.authorName}
+          subheader={`@${post.authorUsername}`}
+        />
       </Link>
-      <h3>{post.authorUsername}</h3>
-      <p>{post.body}</p>
-      <p>{post.createdAt}</p>
-    </div>
+      <CardContent>
+        <Typography variant="body1">{post.body}</Typography>
+        <Typography variant="body2" color="text.secondary">
+          {formatDistance(new Date(post.createdAt), new Date(), {
+            addSuffix: true,
+          })}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton>
+          {user?.likedPosts.includes(post.id) ? (
+            <Favorite />
+          ) : (
+            <FavoriteBorder />
+          )}
+        </IconButton>
+      </CardActions>
+    </Card>
   );
 };
 
